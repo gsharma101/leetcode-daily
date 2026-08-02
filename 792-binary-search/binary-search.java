e986a18ecd0f610706305f52ce353f0f9835c77d
Gaurav Sharma <56496704+gsharma101@users.noreply.github.com>
2026-08-02 06:24:49 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (14.36%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start  = 0;
+        int end = nums.length - 1;
+
+        while(start<=end){
+
+            // 1 To find the middle element
+            int mid = start + (end - start) /2;
+            
+            // 2 When target is less than the middle element
+            if(target < nums[mid]){
+                end = mid - 1;
+            } else if(target > nums[mid]){ 
+            // 3 When target is larger than the bigger element
+                start = mid + 1;
+            }else{
+                return mid; 
+            // 4 Element founded 
+            }
+        }
+
+        return -1;
+    }
+}
